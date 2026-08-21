2f1501177f7cbce3c93755ee2f43e1773207c994
JAJA2004-craeate <jajkamikusiakova@gmail.com>
2026-08-21 13:22:36 +0200

    Update title from 'Edukácia: Montgomery T-tubus' to 'Montgomery T-tubus'

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from google import genai
 # Inicializácia klienta pomocou kľúča zo Secrets
 client = genai.Client(api_key=st.secrets["GOOGLE_API_KEY"])
 
-st.title("Edukácia: Montgomery T-tubus")
+st.title("Montgomery T-tubus")
 st.write("Dobrý deň. Som Váš digitálny asistent pre ošetrovateľskú starostlivosť o Montgomery T-kanylu (MTT). Ako Vám dnes môžem pomôcť?")
 
 query = st.text_input("Napíš svoju otázku...")
